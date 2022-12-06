43e5917f9a641050b9b7065b917e3d7bd88fa233
juinsommer <juinchoisommer@gmail.com>
2022-12-05 20:40:29 -0500

    started a bash script to run the lab demo

--- lab5_pt2.cpp
+++ lab5_pt2.cpp
@@ -1,5 +1,3 @@
-#!/bin/bash
-///bin/true;COMPILER_OPTIONS="-g -Wall -Wextra --std=c++14 -O1 -fsanitize=address,undefined";THIS_FILE="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)/$(basename "${BASH_SOURCE[0]}")";OUT_FILE="/tmp/build-cache/$THIS_FILE";mkdir -p "$(dirname "$OUT_FILE")";test "$THIS_FILE" -ot "$OUT_FILE" || tail -c +12 "$0" | $(which clang++ || which g++) -xc++ $COMPILER_OPTIONS -o "$OUT_FILE" - || exit $?;exec -a "$0" "$OUT_FILE" "$@"
 #include <iostream>
 #include <iostream>
 #include <fstream>
